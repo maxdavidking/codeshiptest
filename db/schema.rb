17eb4674a0d379e0f8aae96313460e15ae69dded
maxdavidking <maxdavidking@gmail.com>
2018-10-24 10:37:19 -0400

    Run rails db:setup and rails db:migrate to setup PG database connections

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_24_141127) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "people", force: :cascade do |t|
     t.string "name"
     t.integer "age"
